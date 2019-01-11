6adb7ecfa661699203039d7a3281399cccc66650
Tim Arney <tim@line37.com>
2019-01-11 15:01:16 -0500

    initial result render

--- src/home/router.js
+++ src/home/router.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { view } from "./view";
 export const router = express.Router();
-router.get("/", function(req, res) {
-  res.send(view);
+router.get("/", async (req, res) => {
+  res.send(await view());
 });
---
+++ src/lib/firestore.js
@@ -0,0 +1,30 @@
+const admin = require("firebase-admin");
+
+let db;
+
+switch (process.env.NODE_ENV) {
+  case "dev":
+    const serviceAccount = require("../../../lighthouse-scanner-firebase-adminsdk.json");
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+      databaseURL: process.env.FIRESTORE_URL
+    });
+    db = admin.firestore();
+    break;
+  default:
+    const functions = require("firebase-functions");
+    admin.initializeApp(functions.config().firebase);
+    db = admin.firestore();
+}
+
+module.exports.loadFromFirestore = async () => {
+  const reposRef = db.collection("scans");
+  const query = reposRef
+    .where("data.timing.total", "<", 10000)
+    .where("data.runtimeError.code", "==", "NO_ERROR");
+  return query.get().then(resp => {
+    var items = [];
+    resp.forEach(r => items.push(r.data()));
+    return items;
+  });
+};
